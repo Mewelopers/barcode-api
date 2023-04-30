c36da2122c897a4ceb340636910b235934d9d770
Ignatella <ignatellasecond@gmail.com>
2023-04-30 21:35:20 +0200

    Fix: enable PKCE in swagger

--- barcode_api/app.py
+++ barcode_api/app.py
@@ -5,7 +5,11 @@ from .config import settings
 from .middleware import ProcessTimeMiddleware
 
 app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    swagger_ui_init_oauth={
+        "usePkceWithAuthorizationCodeGrant": True,
+    },
 )
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.add_middleware(ProcessTimeMiddleware)
